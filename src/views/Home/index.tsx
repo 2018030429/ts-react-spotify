47311dbfee14df95203c7a36b6fc7add6bbb9314
Enrique Contreras <2018030429@upsin.edu.mx>
2021-09-01 12:28:45 -0600

    search design

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './style.css';
+
+import seekerImage from '../../assets/images/seeker.png';
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className="home">
+      <div style={{backgroundImage: `url(${seekerImage})`}} className="home-cover-container" />
+      <h2 className="home-title">Search your favorite song!</h2>
+      <div className="home-searchbox">
+        <input type="text" className="home-searchbox-input" />
+        <button className="home-searchbox-button">Search</button>
+      </div>
     </div>
   );
 }
